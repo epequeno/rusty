7035a6aef7135fe5bc1fce74331c3bce244e861c
Steven Pequeno <steven@pequeno.in>
2018-10-19 12:53:36 -0500

    only send links and let slack handle formatting/expanding

--- src/reader.rs
+++ src/reader.rs
@@ -6,22 +6,23 @@ use slack::Sender;
 
 pub fn read_feed(feed: &str, sender: Sender) {
   let mut channel = Channel::from_url(feed).unwrap();  
-  let mut last_known = channel.into_items()[0].clone();
+  let previous_title = channel.clone().into_items()[0].clone();
+  let mut previous_title = previous_title.title().unwrap().to_string();
   loop {
     channel = Channel::from_url(feed).unwrap();  
     let latest = channel.into_items()[0].clone();
-    if latest.clone() != last_known {
-      let title = latest.clone();
-      let title = title.title().unwrap();
-      let desc = latest.clone();
-      let desc = desc.description().unwrap();
-      let link = latest.clone();
-      let link = link.link().unwrap();
-      let msg = format!("{} {} {}", title, desc, link);
+    let link = latest.clone();
+    let link = link.link().unwrap();
+    let title = latest.clone();
+    let title = title.title().unwrap().clone();
+    if title != previous_title {
+      // we now have a different title than the last time we ran
+      // we'll consider this evidence of an update to the feed.
       // C8EHWNKHV == #rust
-      let _ = sender.send_message("C8EHWNKHV", &msg);
-      last_known = latest;
+      let msg = format!("<{}|{}>", link, title);
+      let _ = sender.send_message("D8S4B7Q8H", &msg);
+      previous_title = title.to_string();
     }
-    thread::sleep(Duration::from_secs(3600));
+    thread::sleep(Duration::from_secs(300));
   }
 }
