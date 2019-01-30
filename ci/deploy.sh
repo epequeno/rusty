eb14d7c7316ba4d5dc6d51ec2ff042a14dbb7514
Steven Pequeno <steven@pequeno.in>
2019-01-30 17:43:09 -0600

    modify travis config again

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo chmod -R ugo+w "$(pwd)"
 docker login -u epequeno -p "$DOCKER_PASSWORD"
+echo "changing dir perms"
+sudo chmod -R ugo+w "$(pwd)"
 docker run --rm -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder cargo build --release
 docker build -t rusty .
 docker tag rusty epequeno/rusty
 docker push epequeno/rusty
-#cargo publish --token $CRATESIO_API_KEY
