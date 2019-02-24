5f62269b1125d5a4da9f7d5396ed3ddc73f49acf
Steven Pequeno <steven@pequeno.in>
2019-02-23 18:16:22 -0600

    add rustfmt to tests

--- ci/test.sh
+++ ci/test.sh
@@ -1,6 +1,11 @@
+# install dependencies
+rustup component add rustfmt
 rustup component add clippy
 cargo install --force cargo-audit
 cargo install --force --git https://github.com/kbknapp/cargo-outdated
+
+# tests
+cargo fmt --all -- --check
 cargo build 
 cargo test --verbose
 cargo clippy -- -D warnings
