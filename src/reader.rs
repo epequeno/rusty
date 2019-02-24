a6875a4dc8f30c4584004e8f09ececdd2557aee7
Steven Pequeno <steven@pequeno.in>
2019-02-24 10:53:30 -0600

    add youtube feeds

--- src/reader.rs
+++ src/reader.rs
@@ -46,6 +46,16 @@ pub struct PythonInsider {
     pub info: FeedInfo,
 }
 
+#[derive(Clone)]
+pub struct TGIK {
+    pub info: FeedInfo,
+}
+
+#[derive(Clone)]
+pub struct JonHoo {
+    pub info: FeedInfo,
+}
+
 impl FeedInfo {
     pub fn new() -> FeedInfo {
         FeedInfo {
@@ -74,6 +84,22 @@ impl Atom {
 impl PythonInsider {
     pub fn new() -> PythonInsider {
         PythonInsider {
+            info: FeedInfo::new(),
+        }
+    }
+}
+
+impl TGIK {
+    pub fn new() -> TGIK {
+        TGIK {
+            info: FeedInfo::new(),
+        }
+    }
+}
+
+impl JonHoo {
+    pub fn new() -> JonHoo {
+        JonHoo {
             info: FeedInfo::new(),
         }
     }
@@ -181,6 +207,87 @@ impl Feed for PythonInsider {
                             url,
                             title: entry.title().into(),
                         }
+                    })
+                    .collect()
+            }
+            None => Vec::new(),
+        }
+    }
+
+    fn get_info(&self) -> FeedInfo {
+        self.info.clone()
+    }
+
+    fn set_info(&mut self, feed_info: FeedInfo) {
+        self.info = feed_info;
+    }
+
+    fn get_previous_titles(&self) -> LinkedHashSet<String> {
+        self.info.clone().previous_titles
+    }
+
+    fn insert_title(&mut self, title: String) {
+        self.info.previous_titles.insert(title);
+    }
+
+    fn pop(&mut self) {
+        self.info.previous_titles.pop_front();
+    }
+}
+
+impl Feed for TGIK {
+    fn read(&self) -> Vec<Article> {
+        match &self.info.url {
+            Some(u) => {
+                let result = reqwest::get(u).unwrap().text().unwrap();
+                let feed: AtomFeed = result.parse().unwrap();
+                feed.entries()
+                    .to_vec()
+                    .iter()
+                    .filter(|entry| entry.title().starts_with("TGI Kubernetes "))
+                    .map(|entry| Article {
+                        url: entry.links().first().unwrap().href().into(),
+                        title: entry.title().into(),
+                    })
+                    .collect()
+            }
+            None => Vec::new(),
+        }
+    }
+
+    fn get_info(&self) -> FeedInfo {
+        self.info.clone()
+    }
+
+    fn set_info(&mut self, feed_info: FeedInfo) {
+        self.info = feed_info;
+    }
+
+    fn get_previous_titles(&self) -> LinkedHashSet<String> {
+        self.info.clone().previous_titles
+    }
+
+    fn insert_title(&mut self, title: String) {
+        self.info.previous_titles.insert(title);
+    }
+
+    fn pop(&mut self) {
+        self.info.previous_titles.pop_front();
+    }
+}
+
+impl Feed for JonHoo {
+    fn read(&self) -> Vec<Article> {
+        match &self.info.url {
+            Some(u) => {
+                let result = reqwest::get(u).unwrap().text().unwrap();
+                let feed: AtomFeed = result.parse().unwrap();
+                feed.entries()
+                    .to_vec()
+                    .iter()
+                    .map(|entry| Article {
+                        url: entry.links().first().unwrap().href().into(),
+                        title: entry.title().into(),
                     })
                     .collect()
             }
